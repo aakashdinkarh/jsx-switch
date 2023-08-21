9c1887525e8d50c9808b81a1df53b391e46b8234
Aakash Dinkar <84908225+aakashdinkarh@users.noreply.github.com>
2023-08-21 12:11:44 +0530

    implement Switch Cases

---
+++ index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export function Case({ children }) {
+	return children;
+}
+export function Default({ children }) {
+	return children;
+}
+
+export function Switch({ children }) {
+	let matchChild = null;
+	let defaultCase = null;
+
+	React.Children.forEach(children, (child) => {
+		if (!matchChild && child.type === Case) {
+			const { condition } = child.props;
+			const conditionResult = Boolean(condition);
+
+			if (conditionResult) {
+				matchChild = child;
+			}
+		} else if (!defaultCase && child.type === Default) {
+			defaultCase = child;
+		}
+	});
+
+	return matchChild ?? defaultCase ?? null;
+}
